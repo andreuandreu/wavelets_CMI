868c0b6b9231e98956a6608d19f70afc6c029354
andreuandreu <andreuaprats@gmail.com>
2022-02-24 12:48:35 +0100

    relative paaath

--- scripts/twin_surrogates.py
+++ scripts/twin_surrogates.py
@@ -4,17 +4,20 @@
 import numpy as np
 from numpy import random
 import sys
-sys.path.append("_ext")
+#sys.path.append(".")
 import pyximport
-pyximport.install()
-from _ext.numerics import _twins_s, _twin_surrogates
+pyximport.install()#._ext/
+
+#from numerics import _twins_s, _twin_surrogates
 #from numerics import  
-#from ._ext.numerics import _embed_time_series_array, _recurrence_plot, \
+print("In module products sys.path[0], __package__ ==", sys.path[0], __package__)
+from ..utils import progressbar
+from ._ext.numerics import _embed_time_series_array, _recurrence_plot#, \
 #    _twins_s, _twin_surrogates, _test_pearson_correlation, \
 #    _test_mutual_information
-
+#from ._ext import numerics
 # easy progress bar handling
-from ..utils import progressbar
+
 
     
 class Surrogates:
